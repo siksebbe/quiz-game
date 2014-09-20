4aa360799c8b9f428ab3a9aa39bd141de2d1cfe2
Aseel Jarjur <jan_fram2000@yahoo.com>
2014-09-20 22:52:17 +0200

    changed questions file

--- js/questions.js
+++ js/questions.js
@@ -1,38 +1,49 @@
-var objH = [{
-	question: 'hej',
-	answer: 'hej'
-}, {
-	question: 'nej',
-	answer: 'nej'
+var objH = [ 
+	{
+		question:'Vem var kungen till Babylon mellan  1792 och 1750 f.Kr? \n 1) Khufu. \n 2) Hammurabi. \n 3) Tutankhamon.',
+		answer:'2'
+	},
+	{
+		question:'När dog Diana prinsessa av Wales? \n 1) 1998. \n 2) 1999. \n 3) 1997.',
+		answer:'3'
 
-}, {
-	question: 'q3h',
-	answer: 'a3h'
+	},
+	{
+		question:'vilken svensk drottning som grävde i st. Perterkyrkan i Rom? \n 1) Kristina. \n 2) Margareta. \n 3) Ulrika Eleonora.',
+		answer:'1'
 
-}, ];
+	},
+	];
+	var objS = [ 
+	{
+		question:'Vem är en av bästa Hocky spelare i Sverige? \n 1) Henrik Lundgvist. \n 2) Christian Eriksen. \n 3) Zlatan Ibrahimovic.',
+		answer:'1'
+	},
+	{
+		question:'På vilket land tog Sverige brons VM i fotboll för damer i 2011? \n 1) London. \n 2) Rom. \n 3) Tyskland.',
+		answer:'3'
 
-var objS = [{
-	question: 'q1s',
-	answer: 'a1s'
-}, {
-	question: 'q2s',
-	answer: 'a2s'
+	},
+	{
+		question:'Från vilket land kommer Diego Maradona? \n 1) USA. \n 2) Argentina. \n 3) Spanien.',
+		answer:'2'
 
-}, {
-	question: 'q3s',
-	answer: 'a3s'
+	},
+	];
 
-}, ];
+	var objM = [ 
+	{
+		question:'Vem som sjunger låten "Alejandro"? \n 1) Lady Gaga. \n 2) Katy Perry. \n 3) Miley Cyrus.',
+		answer:'1'
+	},
+	{
+		question:'på vilket år dog Whitny Housten? \n 1) 2011. \n 2) 2013. \n 3) 2012.',
+		answer:'3'
 
-var objM = [{
-	question: 'q1m',
-	answer: 'a1m'
-}, {
-	question: 'q2m',
-	answer: 'a2m'
+	},
+	{
+		question:'På vilket land dog Mikeal Jackson? \n 1) London. \n 2) Los Angoles. \n 3) Paris.',
+		answer:'2'
 
-}, {
-	question: 'q3m',
-	answer: 'a3m'
-
-}, ];
+	}
+	];
